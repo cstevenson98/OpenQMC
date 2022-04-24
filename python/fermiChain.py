091341598b61598be2e538f4b994041c8ab1826d
Conor Stevenson <conorstevenson0@gmail.com>
2022-04-24 09:45:42 +0100

    electron qutip transport

--- python/fermiChain.py
+++ python/fermiChain.py
@@ -8,13 +8,13 @@ def fermi_tight_binding(n, mu0, J, delta):
     # H0 =  µ0 ∑_i  ci† ci
     H_0 = mu0 * reduce(lambda a, b: a + b, [fermi_lower(n, i).dag() * fermi_lower(n, i) for i in range(n)], zeros_n(n))
 
-    # # Hhop =  -J ∑_i  ci† ci+1 + h.c.
-    # H_hop = reduce(lambda a, b: a + b, [fermi_lower(n, i).dag() * fermi_lower(n, i + 1) for i in range(n - 1)],
-    #                zeros_n(n))
-    # H_hop += -J * H_hop.dag()
-    #
-    # # Hhop =  -∆ ∑_i  ci† ci+1 + h.c.
-    # H_aniso = reduce(lambda a, b: a + b, [fermi_lower(n, i) * fermi_lower(n, i + 1) for i in range(n - 1)], zeros_n(n))
-    # H_aniso += -delta * H_aniso.dag()
+    # Hhop =  -J ∑_i  ci† ci+1 + h.c.
+    H_hop = -J * reduce(lambda a, b: a + b, [fermi_lower(n, i).dag() * fermi_lower(n, i + 1) for i in range(n - 1)],
+                   zeros_n(n))
+    H_hop += H_hop.dag()
 
-    return H_0 #+ H_hop + H_aniso
+    # Hhop =  -∆ ∑_i  ci† ci+1 + h.c.
+    H_aniso = -delta * reduce(lambda a, b: a + b, [fermi_lower(n, i) * fermi_lower(n, i + 1) for i in range(n - 1)], zeros_n(n))
+    H_aniso += H_aniso.dag()
+
+    return H_0 + H_hop + H_aniso
--- python/fermion.py
+++ python/fermion.py
@@ -1,6 +1,47 @@
-from fermiChain import fermi_tight_binding
+import numpy as np
+from matplotlib import pyplot as plt
+from qutip import lindblad_dissipator, fock, tensor, mesolve, steadystate, to_super, expect, operator_to_vector, \
+    sprepost
+
+from fermiChain import fermi_tight_binding, fermi_lower
+from spinOperators import identity_n
+
+
+def comm(a, b):
+    return a*b - b*a
+
+def anticomm(a, b):
+    return a*b + b*a
+
+def supercomm(H, n):
+    return sprepost(H, identity_n(n)) - sprepost(identity_n(n), H)
 
 if __name__ == '__main__':
-    print(fermi_tight_binding(2, 1., 0., 0.))
+    n = 4
 
+    H = fermi_tight_binding(n, 1., 2., 5.)
+    c = [fermi_lower(n, i) for i in range(n)]
 
+    times = np.linspace(0.0, 20.0, 2000)
+    psi0 = tensor([fock(2, 0) for i in range(n)]) + tensor([fock(2, 1) for i in range(n)])
+    psi0 /= psi0.norm()
+
+    collapse_ops = [c[0].dag(), c[3]]
+
+    # Pumping site-1, sink site-N
+    L1 = lindblad_dissipator(c[0].dag(), c[0].dag())
+    L4 = lindblad_dissipator(c[3], c[3])
+
+    rho_ss = steadystate(H, collapse_ops, method='power', use_rcm=True)
+
+    print(c[0].dag() * c[0])
+
+    result = mesolve(H, psi0, times, [L1, L4], [c[0].dag()*c[0], c[1].dag()*c[1], c[2].dag()*c[2], c[3].dag()*c[3]])
+
+    plt.figure()
+    plt.plot(times, result.expect[0])
+    plt.plot(times, result.expect[1])
+    plt.plot(times, result.expect[2])
+    plt.plot(times, result.expect[3])
+
+    plt.show()
